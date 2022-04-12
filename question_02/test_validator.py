5750c46273be48c34ea0714ce4a1e8707e9d9da0
Productivity <prod@pop-os.localdomain>
2022-04-12 12:08:23 -0300

    created tests

---
+++ question_02/test_validator.py
@@ -0,0 +1,24 @@
+import unittest
+from validator import valida_cdvpy
+
+TEST_CASES = (
+        ["123456",  True],
+        ["551234",  False],
+        ["",        False],
+        ["1235",    False],
+        ["12B456",  False],
+        ["a23456",  False],
+        ["695552",  False], #tecnicaly 55 appears 2 times
+        ["027432",  False],
+        ["957213",  True],
+        )
+
+
+class Test_validator(unittest.TestCase):
+    def test_validator(self):
+        self.assertEqual(valida_cdvpy(test_case[0]), testcase[1])
+
+
+if __name__ == "__main__":
+    for test_case in TEST_CASES:
+        unittest.main()
