a0d9a0be3cbccb1b9557d70468ef83764c77c6f7
Productivity <prod@pop-os.localdomain>
2022-04-12 13:18:57 -0300

    done validator

--- question_02/test_validator.py
+++ question_02/test_validator.py
@@ -3,22 +3,23 @@ from validator import valida_cdvpy
 
 TEST_CASES = (
         ["123456",  True],
-        ["551234",  False],
+        ["551234",  True],
         ["",        False],
         ["1235",    False],
         ["12B456",  False],
         ["a23456",  False],
         ["695552",  False], #tecnicaly 55 appears 2 times
         ["027432",  False],
-        ["957213",  True],
+        ["441344",  False],
+        ["957213",  True]
         )
 
 
 class Test_validator(unittest.TestCase):
     def test_validator(self):
-        self.assertEqual(valida_cdvpy(test_case[0]), testcase[1])
+        self.assertEqual(valida_cdvpy(test_case[0]), test_case[1])
 
 
 if __name__ == "__main__":
     for test_case in TEST_CASES:
-        unittest.main()
+        unittest.main(exit=False)
--- question_02/validator.py
+++ question_02/validator.py
@@ -1,7 +1,29 @@
 #! /usr/bin/env python
 
 def valida_cdvpy(cdvpy):
-    raise NotImplementedError
+    
+    if type(cdvpy) != str:    #type == str
+        return False
+    elif not(cdvpy.isdigit()):    #digits 0-9 only
+        return False
+    elif len(cdvpy) != 6:   #length == 6
+        return False
+    elif cdvpy[0] == "0":    #first digit != 0
+        return False
+
+
+    for i in range(5): 
+        counter = 0
+        for j in range(5):
+            if cdvpy[i:i+2] == cdvpy[j:j+2]:
+                counter+=1
+        if counter > 1:
+            return False
+
+
+    return True
+
+
 
 if __name__ == '__main__':
     cdvpy = input('Digite um CDvPy:')
